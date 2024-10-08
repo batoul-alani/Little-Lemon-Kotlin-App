d0f527ee61a06622d128d377124f83b9e7ca37fd
BatoulAlAni <anbatoul@gmail.com>
2024-10-08 17:18:11 +0300

    Handle orientation

--- app/src/main/java/com/example/testrunningapp/MainActivity.kt
+++ app/src/main/java/com/example/testrunningapp/MainActivity.kt
@@ -1,44 +1,80 @@
 package com.example.testrunningapp
 
 import LoginScreen
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.Surface
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.littlelemon.UpperPanel
+import com.example.testrunningapp.ui.theme.Pink80
+import com.example.testrunningapp.ui.theme.Purple80
+import com.example.testrunningapp.ui.theme.PurpleGrey80
 import com.example.testrunningapp.ui.theme.TestRunningAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             TestRunningAppTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    LoginScreen()
-                }
+                Scaffold (
+
+                    topBar =
+                    {
+                        TopAppBar(
+                            title = {
+                                Text(resources.getString(R.string.app_name))
+                            },
+                            )
+                    },
+
+                    content = {
+                            paddingValues ->
+                        MenuContent(paddingValues = paddingValues)
+                    }
+                )
+
             }
         }
     }
@@ -51,4 +87,88 @@ fun HomeScreen(){
     }
 }
 
+@Composable
+fun MenuContent(paddingValues: PaddingValues){
+    val configuration = LocalConfiguration.current
+    val rotationValue = configuration.orientation
 
+    Surface (modifier = Modifier.padding(paddingValues)) {
+        val menuPadding = 8.dp
+        when(rotationValue){
+            ORIENTATION_LANDSCAPE -> {
+                Column {
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Appetizers",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Purple80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Salads",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .padding(menuPadding)
+                        )
+                    }
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Drinks",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Pink80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Mains",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(PurpleGrey80)
+                                .padding(menuPadding)
+                        )
+                    }
+                }
+            }
+            else -> {
+                Column {
+                    Text(
+                        "Appetizers",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Purple80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Salads",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Drinks",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Pink80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Mains",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(PurpleGrey80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
